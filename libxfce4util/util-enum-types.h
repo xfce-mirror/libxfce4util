e1dd2a072f1d7ceac6f0cd62a958ab1d1ef5786c
Danny Milosavljevic <danny_milo@yahoo.com>
2005-05-29 14:34:33 +0000

    use XFCE_TYPE_* for enum types to calm down codegenerator
    (Old svn revision: 14602)

--- libxfce4util/util-enum-types.h
+++ libxfce4util/util-enum-types.h
@@ -7,7 +7,7 @@
 /* enumerations from "xfce-resource.h" */
 
 GType xfce_resource_type_get_type (void);
-#define UTIL_TYPE_RESOURCE_TYPE (xfce_resource_type_get_type())
+#define XFCE_TYPE_RESOURCE_TYPE (xfce_resource_type_get_type())
 
 #endif /* __XFCE_UTIL_ENUM_TYPES_H__ */
 
