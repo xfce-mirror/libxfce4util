21cf66ae91921cc9bd2ea42662d38f02abf5db07
Danny Milosavljevic <danny_milo@yahoo.com>
2005-05-29 14:11:36 +0000

    add util enum types (i.e. one :))
    (Old svn revision: 14595)

---
+++ libxfce4util/util-enum-types.h
@@ -0,0 +1,15 @@
+
+/* Generated data (by glib-mkenums) */
+
+#ifndef __XFCE_UTIL_ENUM_TYPES_H__
+#define __XFCE_UTIL_ENUM_TYPES_H__
+
+/* enumerations from "xfce-resource.h" */
+
+GType xfce_resource_type_get_type (void);
+#define UTIL_TYPE_RESOURCE_TYPE (xfce_resource_type_get_type())
+
+#endif /* __XFCE_UTIL_ENUM_TYPES_H__ */
+
+/* Generated data ends here */
+
