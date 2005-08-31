79ffe4d962d7037c43b80c13bbc4826e2d20b446
Benedikt Meurer <benny@xfce.org>
2005-08-31 09:58:14 +0000

    2005-08-31	Benedikt Meurer <benny@xfce.org>
    	* autogen.sh, configure.in.in: Merge build framework changes from
    	  libexo.
    
    
    
    
    (Old svn revision: 17216)

--- autogen.sh
+++ autogen.sh
@@ -18,6 +18,11 @@ EOF
   exit 1
 }
 
+# substitute revision and date
+revision=`svn info $0 | awk '/^Revision: / {printf "%04d\n", $2}'`
+sed -e "s/@DATE@/`date +%Y%m%d`/g" -e "s/@REVISION@/${revision}/g" \
+  < "configure.in.in" > "configure.in"
+
 exec xdt-autogen $@
 
 # vi:set ts=2 sw=2 et ai:
