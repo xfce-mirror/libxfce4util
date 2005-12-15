78487ae2b6912c5b0254a6d0ca6caab29f9fb2a3
Danny Milosavljevic <danny_milo@yahoo.com>
2005-12-15 22:29:08 +0000

    fix bug in xfce-rc-simple.c when you have a "desktop file" which has a key with a language suffix but no generic key: libxfce4util-CRITICAL **: simple_add_entry: assertion ocale == NULL' failed aborting... Aborted
    (Old svn revision: 19044)

--- libxfce4util/xfce-rc-simple.c
+++ libxfce4util/xfce-rc-simple.c
@@ -179,7 +179,28 @@ simple_add_entry (XfceRcSimple *simple,
 
   if (G_UNLIKELY (entry == NULL))
     {
-      g_return_val_if_fail (locale == NULL, NULL);
+      if (locale != NULL) {
+        /* this point is reached when there is a .desktop file that lists a language specific key for something that had no language-neutral key yet.
+           Example:
+             b.desktop
+             
+             [Desktop Entry]
+             Version=1.0
+             Encoding=UTF-8
+             Name=xyz
+             GenericName[de_AT]=Test
+             
+           here GenericName[de_AT] will end up here.
+           The previous way with g_return_val_if_fail would call an assertion failure and terminate the _whole application_(!!).
+           
+           Saner ways to react are either just ignoring GenericName[de_AT] altogether, or, alternatively, just set the normal GenericName
+           to Test too (i.e. imply GenericName=Test).
+           
+           For now, we are just ignoring the line altogether. But we aren't assert-failing anymore and the apps dont crash anymore.
+           */
+        return NULL;
+      }
+      /* why you annoying macro, will you stop borking libxfceutil? thanks. DO NOT DO THAT: g_return_val_if_fail (locale == NULL, NULL); */
 
       entry         = g_chunk_new (Entry, simple->entry_chunk);
       entry->key    = g_string_chunk_insert (simple->string_chunk, key);
