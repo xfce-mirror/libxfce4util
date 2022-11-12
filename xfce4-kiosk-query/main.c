b5047b47302e54003d00ea23364f8c1bc715acd0
Alexander Schwinn <alexxcons@xfce.org>
2022-11-12 22:08:15 +0100

    bump copyright to 2022

--- xfce4-kiosk-query/main.c
+++ xfce4-kiosk-query/main.c
@@ -68,7 +68,7 @@ main (int argc, char **argv)
         {
         case 'v':
           printf ("xfce4-kiosk-query %s (Xfce %s)\n\n"
-                  "Copyright (c) 2003-2004\n"
+                  "Copyright (c) 2003-2022\n"
                   "        The Xfce development team. All rights reserved.\n"
                   "Written for Xfce by Benedikt Meurer <benny@xfce.org>.\n\n"
                   "Please report bugs to <%s>.\n",
