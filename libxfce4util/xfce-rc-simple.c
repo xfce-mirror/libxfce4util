xfce-mirror/libxfce4util

commit 3a279d2b5fd5315e0273bf2ced12dcbb9ab9de46

xfce-rc: Add support for the LANGUAGE environment variable

libxfce4util/xfce-rc-simple.c
@@ -552,6 +552,26 @@ simple_group_free (Group *group)
 
 
 
+static guint
+xfce_locale_match_rc (const XfceRc *rc, const gchar *locale)
+{
+  if (rc->languages != NULL)
+    {
+      for (gchar **plng = rc->languages; *plng != NULL; plng++)
+        {
+          guint match = xfce_locale_match (*plng, locale);
+          if (match > XFCE_LOCALE_NO_MATCH)
+            return match;
+        }
+    }
+  else if (rc->locale != NULL)
+    return xfce_locale_match (rc->locale, locale);
+
+  return XFCE_LOCALE_NO_MATCH;
+}
+
+
+
 XfceRcSimple*
 _xfce_rc_simple_new (XfceRcSimple *shared,
                      const gchar  *filename,
@@ -649,10 +669,10 @@ _xfce_rc_simple_parse (XfceRcSimple *simple)
           continue;
         }
 
-      if (rc->locale == NULL)
+      if (rc->locale == NULL && rc->languages == NULL)
         continue;
 
-      if (!readonly || xfce_locale_match (rc->locale, locale) > XFCE_LOCALE_NO_MATCH)
+      if (!readonly || xfce_locale_match_rc (rc, locale) > XFCE_LOCALE_NO_MATCH)
         simple_add_entry (simple, key, value, locale);
     }
 
@@ -1002,14 +1022,14 @@ _xfce_rc_simple_read_entry (const XfceRc *rc,
   if (G_LIKELY (entry != NULL))
     {
       /* check for localized entry (best fit!) */
-      if (G_LIKELY (translated && rc->locale != NULL))
+      if (G_LIKELY (translated && (rc->locale != NULL || rc->languages != NULL)))
         {
           best_match = XFCE_LOCALE_NO_MATCH;
           best_value = NULL;
 
           for (lentry = entry->lfirst; lentry != NULL; lentry = lentry->next)
             {
-              match = xfce_locale_match (rc->locale, lentry->locale);
+              match = xfce_locale_match_rc (rc, lentry->locale);
               if (match == XFCE_LOCALE_FULL_MATCH)
                 {
                   /* FULL MATCH */

libxfce4util/xfce-rc-private.h
@@ -61,6 +61,7 @@ struct _XfceRc
                                  const gchar  *value);
 
   gchar   *locale;
+  gchar  **languages;
 };
 
 #define XFCE_RC_CONFIG(obj)       ((XfceRcConfig *) (obj))

libxfce4util/xfce-rc.c
@@ -88,12 +88,17 @@ void
 _xfce_rc_init (XfceRc *rc)
 {
 #ifdef HAVE_SETLOCALE
+  const gchar *language;
   gchar *locale;
 #endif
 
   _xfce_return_if_fail (rc != NULL);
 
 #ifdef HAVE_SETLOCALE
+  language = g_getenv ("LANGUAGE");
+  if (language != NULL)
+    rc->languages = g_strsplit (language, ":", -1);
+
   locale = setlocale (LC_MESSAGES, NULL);
   if (locale != NULL
       && strcmp (locale, "C") != 0
@@ -208,6 +213,9 @@ xfce_rc_close (XfceRc *rc)
 
   if (rc->locale != NULL)
     g_free (rc->locale);
+
+  if (rc->languages != NULL)
+    g_strfreev (rc->languages);
 
   g_free (rc);
 }

commit b2a2aee40956e194944031432d7147c0d5613e93

xfce-rc: Fix support for the LANGUAGE environment variable
When LANGUAGE contained more than one language code and there was a
translation to the first language in a .desktop file, a translation to
the second language was returned in the case when the line for the
second language was above the line for the first one. Now translation
for the first language is returned regardless of the line order. The
`entry != NULL` condition is inverted to make the diff smaller and
reduce the amount of indentation.

Fixes: 3a279d2b5fd5315e0273bf2ced12dcbb9ab9de46

libxfce4util/xfce-rc-simple.c
@@ -1019,17 +1019,23 @@ _xfce_rc_simple_read_entry (const XfceRc *rc,
     if (str_is_equal (entry->key, key))
       break;
 
-  if (G_LIKELY (entry != NULL))
+  if (G_UNLIKELY (entry == NULL))
+    return NULL;
+
+  /* check for localized entry (best fit!) */
+  if (G_LIKELY (translated && (rc->locale != NULL || rc->languages != NULL)))
     {
-      /* check for localized entry (best fit!) */
-      if (G_LIKELY (translated && (rc->locale != NULL || rc->languages != NULL)))
+      gchar *locale_languages[] = { rc->locale, NULL };
+
+      for (gchar **p = (rc->languages != NULL) ? rc->languages : locale_languages;
+            *p != NULL; p++)
         {
           best_match = XFCE_LOCALE_NO_MATCH;
           best_value = NULL;
 
           for (lentry = entry->lfirst; lentry != NULL; lentry = lentry->next)
             {
-              match = xfce_locale_match_rc (rc, lentry->locale);
+              match = xfce_locale_match (*p, lentry->locale);
               if (match == XFCE_LOCALE_FULL_MATCH)
                 {
                   /* FULL MATCH */
@@ -1047,11 +1053,9 @@ _xfce_rc_simple_read_entry (const XfceRc *rc,
 
           /* FALL-THROUGH */
         }
-
-      return entry->value;
     }
 
-  return NULL;
+  return entry->value;
 }
 
 